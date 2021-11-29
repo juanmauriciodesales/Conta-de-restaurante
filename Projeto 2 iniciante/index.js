7b7ab764af8ca0e90fefd25e50f2923f95779a08
juanmauriciodesales <juanmauriciodesales@gmail.com>
2021-11-29 19:05:09 -0300

    Subindo projeto
    Subindo HTML,CSS e Javascript

---
+++ Projeto 2 iniciante/index.js
@@ -0,0 +1,29 @@
+var cont=0
+var lista=[]
+var soma=0
+var contador=document.getElementById('contador')
+var spamTotal=document.getElementById("somar")
+var input=document.getElementById('exampleInputEmail1')
+
+function adicionarIten(){
+    if(input.value.length!==0){
+        cont++
+        lista.push(parseFloat(input.value))
+        contador.innerHTML = cont
+        input.value=''
+        
+    }
+}
+
+function somarTotal(){
+    var controleDoLaço=0
+    if(cont!==0){
+        while(controleDoLaço<lista.length){
+         soma+=lista[controleDoLaço]
+         controleDoLaço++
+        }
+        spamTotal.innerHTML= soma.toFixed(2)
+
+    }
+
+}
